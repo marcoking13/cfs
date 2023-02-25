919d2f679d4b7b82a7ee5caf0040a57f273c4b2c
marcoking13 <marcokhodr16@gmail.com>
2023-02-24 23:28:34 -0700

    l;

--- public/assets/js/navbar_render.js
+++ public/assets/js/navbar_render.js
@@ -24,14 +24,31 @@ var desktop_nav = `
 `;
 var mobile_nav = `
 <div class="pos-f-t background-1a">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="p-4">
-      <h4 class="text-white">Collapsed content</h4>
-      <span class="text-muted">Toggleable via the navbar brand.</span>
+  <div class="collapse" id="navbarToggleExternalContent" status="inactive">
+
+    <div class="row navbar_home_mobile mt2_5">
+      <div class="col-1 "></div>
+      <div class="col-10">
+      <div class="row">
+      <div class="col-12">
+        <a href="/"><p class="link_nav medium-font">Home</p></a>
+      </div>
+
+      <div class="col-12">
+        <a href = "/about"><p class="link_nav medium-font">About Us</p></a>
+      </div>
+      <div class="col-12">
+        <p class="link_nav medium-font">Contact Us</p>
+      </div>
+      <div class="col-12">
+        <p class="link_nav medium-font">Schedule Quote</p>
+      </div>
+      </div>
+      </div>
     </div>
   </div>
   <nav class="navbar navbar-dark background-1a">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button id="button_navbar" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
   </nav>
@@ -45,5 +62,24 @@ if(width >=844){
 }else{
   navbar_element.innerHTML = mobile_nav;
 }
+
+var nav_button = document.getElementById("button_navbar");
+nav_button.addEventListener("click",()=>{
+  var nav_container = document.getElementById("navbarToggleExternalContent");
+  var toggle = "inactive";
+  if(nav_container.getAttribute("status") == "inactive"){
+    toggle = "active";
+  }else{
+    toggle = "inactive";
+  }
+  nav_container.setAttribute("status",toggle);
+  if(nav_container.getAttribute("status") == "inactive"){
+    nav_container.classList.add("active-navbar");
+    nav_container.classList.remove("collapse");
+  }else{
+    nav_container.classList.add("collapse");
+    nav_container.classList.remove("active-navbar");
+  }
+});
 
 document.getElementById('vid').play();
