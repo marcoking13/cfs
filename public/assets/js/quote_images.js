849c2250fbfae53477e742deacba347c02157534
marcoking13 <marcokhodr16@gmail.com>
2023-03-11 22:15:14 -0700

    k

---
+++ public/assets/js/quote_images.js
@@ -0,0 +1,11 @@
+var quote_image_background = document.getElementById("quote_image_what_we_do");
+var quote_showcase_background = document.getElementById("quote_showcase_background");
+
+if(window.innerWidth <= 844){
+  quote_image_background.setAttribute("src","./assets/mobile_what_we_do_2.png");
+  quote_showcase_background.setAttribute("src","./assets/mobile_quote_showcase.png");
+
+}else{
+  quote_image_background.setAttribute("src","./assets/services.png");
+  quote_showcase_background.setAttribute("src","./assets/showcase_3.png");
+}
