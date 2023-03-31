6af0b67cd48ac074d4df48f5c3bb5c1bb270abb4
marcoking13 <marcokhodr16@gmail.com>
2023-03-30 17:27:32 -0700

    l

---
+++ public/assets/js/home_showcase.js
@@ -0,0 +1,7 @@
+
+function switch_image(query,src_name){
+  var img = document.getElementById(query);
+  img.setAttribute("src",src_name);
+}
+
+switch_image("mobile_showcase","./assets/images/home_mobile.png")
---
+++ public/assets/js/mobile_image_switch.js
@@ -0,0 +1,4 @@
+function switch_image(query,src_name){
+  var img = document.findElementById(query);
+  img.setAttribute("src",src_name);
+}
