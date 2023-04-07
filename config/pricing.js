39564d4ed8588e24527b2920630614f5478b6742
marcoking13 <marcokhodr16@gmail.com>
2023-04-06 20:25:43 -0700

    l

--- config/pricing.js
+++ config/pricing.js
@@ -1,23 +1,23 @@
 const price_sheet = [
    {
     key:"small",
-    price:1.5,
+    price:2,
     price_half:1
   },
   {
   key:"medium",
-  price:2,
+  price:3,
   price_half:1.5
   },
   {
   key:"large",
-  price:4,
+  price:5,
   price_half:2.5
 },
   {
   key:"screens",
-  price:5,
-  price_half:5
+  price:6,
+  price_half:6
   }
 ]
 
