7b11c6bef97f1b9acbae2e1e720d89d552996c55
marcoking13 <marcokhodr16@gmail.com>
2023-06-02 17:59:54 -0700

    L

--- public/assets/js/home_showcase.js
+++ public/assets/js/home_showcase.js
@@ -4,10 +4,11 @@ function switch_image(query,src_name){
   img.setAttribute("src",src_name);
 }
 
-if(window.innerWidth <=844){
+if(window.matchMedia("(orientation: landscape)").matches){
+   switch_image("mobile_showcase","./assets/images/showcase_landscape.png")
+}
+
+else if(window.innerWidth <=844){
 
   switch_image("mobile_showcase","./assets/images/home_mobile.png")
-}
- if(window.innerHeight < window.innerWidth){
-    switch_image("mobile_showcase","./assets/images/showcase_landscape.png")
 }
