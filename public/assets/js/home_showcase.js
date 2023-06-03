d358f62516cee829efaf943af739b7c4d788dfee
marcoking13 <marcokhodr16@gmail.com>
2023-06-02 18:16:50 -0700

    L

--- public/assets/js/home_showcase.js
+++ public/assets/js/home_showcase.js
@@ -4,10 +4,7 @@ function switch_image(query,src_name){
   img.setAttribute("src",src_name);
 }
 
-if(window.innerHeight <=  window.innerWidth){
-  console.log(window.innerHeight <=  window.innerWidth);
-   switch_image("mobile_showcase","./assets/images/showcase_pad.png")
-}
+
 
 else if(window.innerWidth <=844){
 
