15e6c5e6c62a0347d22e969b88f30b46f9c8278b
marcoking13 <marcokhodr16@gmail.com>
2023-11-05 20:00:52 -0700

    L

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 let _db;
-var url ="mongodb+srv://marcokhodr116:thirdpi1@cluster0.9nkwh9b.mongodb.net/?retryWrites=true&w=majority" || "mongodb://localhost:27017/" ;
+var url_2 = "mongodb://marcokhodr16@gmail.com:Thirdpi1@iad2-c9-2.mongo.objectrocket.com:52798/cfs?replicaSet=2be5fbe4b39d43b5852229afe35aa089";
+var url =  "mongodb://localhost:27017/" ;
 
 const MongoConnect = (cb) => {
 
